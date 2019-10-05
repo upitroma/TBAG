ed8bc3a577d286afa19d89b6d41db572828c067c
upitroma <allen@rudolphweb.com>
2019-10-04 21:57:14 -0500

    run NodeGenerationTest to see connections

--- NodeTesting/Node.java
+++ NodeTesting/Node.java
@@ -1,3 +1,25 @@
 public class Node{
-    
+    public int id;
+    public int[] neighborIDs;
+
+    public Node(int id, int[] neighborIDs){
+        this.id = id;
+        this.neighborIDs = neighborIDs;
+    }
+    public Node(int id){
+        this.id = id;
+    }
+
+    public int getID(){
+        return this.id;
+    }
+    public int[] getNeighborIDs(){
+        return this.neighborIDs;
+    }
+    public void setID(int newID){
+        this.id = newID;
+    }
+    public void setNeighborIDs(int[] newNeighbors){
+        this.neighborIDs = newNeighbors;
+    }
 }
--- NodeTesting/NodeGenerationTest.java
+++ NodeTesting/NodeGenerationTest.java
@@ -1,5 +1,46 @@
+import sun.jvm.hotspot.gc.parallel.PSYoungGen;
+
 public class NodeGenerationTest{
+
+    public static Node[] nodeMap;
+    public static int maxNeighbors = 4;
     public static void main(String[] args){
         
+        for (int i = 0; i<100; i++){
+            System.out.print(i + " n: [");
+            System.out.print(psudorandom(i)+ " ");
+            System.out.print("] [");
+
+            //lower connections
+            for(int b = i-1; b>i-6;b--){
+                if(b+psudorandom(b)>=i){
+                    System.out.print(" " + b + " ");
+                }
+            }
+
+            //higher connections
+            for(int t = i+1; t<=i+psudorandom(i); t++){
+                System.out.print(" " + t + " ");
+            }
+            System.out.println("]");
+        }
+    }
+
+
+    public static int psudorandom(int seed){
+        //https://www.geeksforgeeks.org/pseudo-random-number-generator-prng/
+        int m = 11;
+        int a = 6;
+        int c = 3;
+        return (int) (((((a*seed)+c)%m)/10)*maxNeighbors)+1;
+    }
+    
+    public static Node[] addItemToArray(Node[] arr, Node input){
+        Node[] temp = new Node[arr.length+1];
+        for (int i = 0; i<arr.length; i++){
+            temp[i] = arr[i];
+        }
+        temp[arr.length] = input;
+        return temp;
     }
 }
